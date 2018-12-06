d74339378835f417e45c8ee59c8a16dd4c3ecc0d
admin <761597917@qq.com>
2018-12-06 11:40:53 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','关于')
+
+@section('content')
+    <h1>主页</h1>
+@endsection
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('title','帮助')
+
+
+@section('content')
+    <h1>主页</h1>
+@endsection
